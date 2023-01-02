47f74c5f4866ad7fb3e667608fac0dd1a77b3e17
antmsbrito <50997716+antmsbrito@users.noreply.github.com>
2023-01-02 15:47:22 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,10 +1,43 @@
 # Cell Averager
-### Set of tools to build an heatmap of fluorescence given a eHooke report
-#### A smaller version of this tool is implemented in https://github.com/antmsbrito/eHooke_1.0
-#### Built in Python 3.6 
+## Averaging fluorescence in _S. aureus_ cells
+
+Cell Averager is a tool to build an heatmap of the fluorescence of an average _S. aureus_ cells. The code heavily relies on [eHooke's][1] capabilities to segment cells.
+
+Given a set of microscopy images the tool automatically runs eHooke in order to segment cells, orients each cell according to their
+major axis and averages the fluorescence in order to build a model cell. It requires per FoV a base image that eHooke can segment (phase, brightfield or membrane dye) and it works with 1 or 2 fluorescence channels.
+
+If spot detection was done using [TrackMate][2] and the output .xml file is provided it can
+also build an heatmap of the average spot localization.
+
+This tool (without spot detection support) is implemented in [eHooke][1].
 
 
-### Requirements
-#### - eHooke ('https://github.com/antmsbrito/eHooke_1.0')
+## Requirements
+####  Built in Python 3.6
+#### - eHooke (https://github.com/antmsbrito/eHooke_1.0)
 
-### Usage instructions: 
+## Usage instructions
+
+#### - Organize image data. 
+```
+Experiment 
+│
+└───FoV_1
+│   ├──   Fluor_1.tif
+│   ├──   Fluor_1.xml (optional)
+│   ├──   Fluor_2.tif
+│   ├──   Fluor_1.xml (optional)
+│   └──   Base.tif
+└───FoV_1
+│   ├──   Fluor_1.tif
+│   ├──   Fluor_1.xml (optional)
+│   ├──   Fluor_2.tif
+│   ├──   Fluor_1.xml (optional)
+│   └──   Base.tif
+└─── ...
+
+```
+#### - 
+
+[1]: https://github.com/antmsbrito/eHooke_1.0
+[2]: doi:10.1038/s41592-022-01507-1
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import sys
+import tkinter as tk
 
-sys.path.append(r'C:\Users\António\Documents\GitHub\eHooke_1.0')
+from GUI import MainGUI
 
 import numpy as np
 import matplotlib
@@ -10,30 +11,36 @@ from skimage.external import tifffile
 from skimage.io import imsave
 from skimage.color import gray2rgb
 
-from images import ImageManager
-from parameters import MaskParameters, RegionParameters, CellParameters, ParametersManager
-from segments import SegmentsManager
-from cells import CellManager
-
 from cellmodeler import CellModeler
 from cellaligner import CellAligner
 
 
-def runCA(phase, fluor, savepath):
+def run_CellAverager(base, fluor, basetype):
     """
-    Given a set of images run ehooke analysis
+    Runs ehooke segmentation on two images and saves the label image
+    Aligns cells and calculates cell model average
+    :param base: Path to base image
+    :param fluor: Path to fluor image
+    :param basetype: One of Membrane, BF or Phase
+    :return:
     """
     if not os.path.exists(fluor):
         return 0
 
-    # Set params
     par = ParametersManager()
 
-    par.imageloaderparams.mask_fill_holes = True
+    if basetype == "Phase":
+        par.imageloaderparams.mask_fill_holes = True
+    elif basetype == "BF":
+        par.imageloaderparams.mask_algorithm = 'StarDist_BF'
+    elif basetype == "Membrane":
+        par.imageloaderparams.mask_algorithm = 'StarDist'
+    else:
+        raise ValueError(f"Only Phase, BF or Membrane are valid base image types. You provided {basetype}")
 
-    # Load images
+    # Load images and compute mask
     imm = ImageManager()
-    imm.load_base_image(phase, par.imageloaderparams)
+    imm.load_base_image(base, par.imageloaderparams)
     imm.compute_base_mask(par.imageloaderparams)
     imm.compute_mask(par.imageloaderparams)
     imm.load_fluor_image(fluor, par.imageloaderparams)
@@ -41,102 +48,146 @@ def runCA(phase, fluor, savepath):
     seg = SegmentsManager()
     seg.compute_segments(par.imageprocessingparams, imm)
 
-    seg.save_labels(filename=os.path.join(os.path.dirname(phase), 'Labels'))
+    seg.save_labels(filename=os.path.join(os.path.dirname(base), 'Labels'))
 
-    # TODO CellManager and process cells needs parameters manager instance
-    # All other functions do not and instead take parameters individually
-    # Fix inconsistency on ehooke source code, either take parmanager or individual pars
     cel = CellManager(par)
     cel.compute_cells(par, imm, seg)
     cel.process_cells(par.cellprocessingparams, imm)
     cel.overlay_cells(imm)
+    # end of ehooke
 
+    # Cell alignment
     cellalligner = CellAligner(cel)
     cellalligner.align_cells()
 
+    # Cell model
+    savepath = fluor[:-4] + "_CA_"
     cellmodeler = CellModeler(cel)
-    cellmodeler.create_cell_model_from_TM(fluor.replace('.tif', '.xml'), savepath, imm.align_values)
 
-    return len(cel.cells)
+    if os.path.exists(fluor.replace('.tif', '.xml')):
+        cellmodeler.create_cell_model_from_TM(fluor.replace('.tif', '.xml'), savepath, imm.align_values)
+    else:
+        cellmodeler.create_cell_model_from_ehooke(savepath)
 
-
-def run_workflow(folder):
-
-    ## Terminus
-    nt = runCA(folder + r"\Phase.tif",
-               folder + r"\CFP.tif",
-               folder + r"\CFP_hm")
-
-    ## Origin
-    no = runCA(folder + r"\Phase.tif",
-               folder + r"\YFP.tif",
-               folder + r"\YFP_hm")
-
-    return nt
+    return 1
 
 
-def run_batch_workflow(folder):
-    total_cells = 0
+def run_batch_workflow(folder, fluor_names, base_name, base_type):
     for replicate in os.listdir(folder):
         replicate_fullname = os.path.join(folder, replicate)
         if os.path.isdir(replicate_fullname):
-            n = run_workflow(replicate_fullname)
-            total_cells += n
+            base_path = os.path.join(replicate_fullname, base_name + '.tif')
+            for fname in fluor_names:
+                fluor_path = os.path.join(replicate_fullname, fname + '.tif')
+                run_CellAverager(base_path, fluor_path, base_type)
 
-    print(folder, total_cells)
 
+def join_replicates(folder, fluor_names):
 
-def join_replicates(folder, spot_condition):
-    all_cfp = []
-    all_yfp = []
-    N_cfp = []
-    N_yfp = []
+    all_fluor1 = []
+    N_fluor1 = []
+    all_fluor1_TM = []
+    N_fluor1_TM = []
+
+    all_fluor2 = []
+    N_fluor2 = []
+    all_fluor2_TM = []
+    N_fluor2_TM = []
 
     for replicate in os.listdir(folder):
         replicate_fullname = os.path.join(folder, replicate)
 
         if os.path.isdir(replicate_fullname):
-            if os.path.exists(os.path.join(replicate_fullname, "CFP_hm" + spot_condition + ".tif")):
-                with tifffile.TiffFile(replicate_fullname + os.sep + "CFP_hm" + spot_condition + ".tif") as tif:
-                    all_cfp.append(tif.asarray())
-                    N_cfp.append(int(tif.pages[0].tags["image_description"].value))
+            fluor1name = os.path.join(replicate_fullname, fluor_names[0]+"_CA_model_greater_1"+".tif")
+            fluor1name_tm = os.path.join(replicate_fullname, fluor_names[0]+"_CA_model_greater_1_TM_SPOTS"+".tif")
+            fluor2name = os.path.join(replicate_fullname, fluor_names[1]+"_CA_model_greater_1"+".tif")
+            fluor2name_tm = os.path.join(replicate_fullname, fluor_names[1]+"_CA_model_greater_1_TM_SPOTS"+".tif")
 
-            if os.path.exists(os.path.join(replicate_fullname, "YFP_hm" + spot_condition + ".tif")):
-                with tifffile.TiffFile(replicate_fullname + os.sep + "YFP_hm" + spot_condition + ".tif") as tif:
-                    all_yfp.append(tif.asarray())
-                    N_yfp.append(int(tif.pages[0].tags["image_description"].value))
+            if os.path.exists(fluor1name):
+                with tifffile.TiffFile(fluor1name) as tif:
+                    all_fluor1.append(tif.asarray())
+                    N_fluor1.append(int(tif.pages[0].tags["image_description"].value))
+                with tifffile.TiffFile(fluor1name_tm) as tif:
+                    all_fluor1_TM.append(tif.asarray())
+                    N_fluor1_TM.append(int(tif.pages[0].tags["image_description"].value))
 
-    cumsum_cfp = np.zeros((30, 30))
-    for idx, N in enumerate(N_cfp):
-        cumsum_cfp += resize(all_cfp[idx] * N, (30, 30))
+            if os.path.exists(fluor2name):
+                with tifffile.TiffFile(fluor2name) as tif:
+                    all_fluor2.append(tif.asarray())
+                    N_fluor2.append(int(tif.pages[0].tags["image_description"].value))
+                with tifffile.TiffFile(fluor2name_tm) as tif:
+                    all_fluor2_TM.append(tif.asarray())
+                    N_fluor2_TM.append(int(tif.pages[0].tags["image_description"].value))
 
-    cumsum_yfp = np.zeros((30, 30))
-    for idx, N in enumerate(N_yfp):
-        cumsum_yfp += resize(all_yfp[idx] * N, (30, 30))
+    cumsum_fluor1 = np.zeros((30, 30))
+    for idx, N in enumerate(N_fluor1):
+        cumsum_fluor1 += resize(all_fluor1[idx] * N, (30, 30))
+    cumsum_fluor2 = np.zeros((30, 30))
+    for idx, N in enumerate(N_fluor2):
+        cumsum_fluor2 += resize(all_fluor2[idx] * N, (30, 30))
+
+    cumsum_fluor1_TM = np.zeros((30, 30))
+    for idx, N in enumerate(N_fluor1_TM):
+        cumsum_fluor1_TM += resize(all_fluor1_TM[idx] * N, (30, 30))
+    cumsum_fluor2_TM = np.zeros((30, 30))
+    for idx, N in enumerate(N_fluor2_TM):
+        cumsum_fluor2_TM += resize(all_fluor2_TM[idx] * N, (30, 30))
 
     cellmodeler = CellModeler(None)
     colormap = matplotlib.cm.get_cmap("coolwarm")
 
-    if len(N_cfp) > 0:
-        imsave(folder + os.sep + f"model_cfp_{spot_condition}.tif", cumsum_cfp / sum(N_cfp), plugin="tifffile")
-        color_img = np.zeros((30, 30, 3))
-        color_cfp = cellmodeler.assign_color(cumsum_cfp / sum(N_cfp), color_img, colormap)
-        imsave(folder + os.sep + f"color_cfp_{spot_condition}_n{sum(N_cfp)}.png", color_cfp)
+    if len(N_fluor1) > 0:
+        imsave(folder + os.sep + f"model_{fluor_names[0]}.tif", cumsum_fluor1 / sum(N_fluor1), plugin="tifffile")
+        color_img1 = np.zeros((30, 30, 3))
+        color_fluor1 = cellmodeler.assign_color(cumsum_fluor1 / sum(N_fluor1), color_img1, colormap)
+        imsave(folder + os.sep + f"color_{fluor_names[0]}_n{sum(N_fluor1)}.png", color_fluor1)
 
-    if len(N_yfp) > 0:
-        imsave(folder + os.sep + f"model_yfp_{spot_condition}.tif", cumsum_yfp / sum(N_yfp), plugin="tifffile")
-        color_img = np.zeros((30, 30, 3))
-        color_yfp = cellmodeler.assign_color(cumsum_yfp / sum(N_yfp), color_img, colormap)
+    if len(N_fluor1_TM) > 0:
+        imsave(folder + os.sep + f"model_{fluor_names[0]}_TM_.tif", cumsum_fluor1_TM / sum(N_fluor1_TM), plugin="tifffile")
+        color_img1_TM = np.zeros((30, 30, 3))
+        color_fluor1_TM = cellmodeler.assign_color(cumsum_fluor1_TM / sum(N_fluor1_TM), color_img1_TM, colormap)
+        imsave(folder + os.sep + f"color_{fluor_names[0]}_TM_n{sum(N_fluor1_TM)}.png", color_fluor1_TM)
 
-        imsave(folder + os.sep + f"color_yfp_{spot_condition}_n{sum(N_yfp)}.png", color_yfp)
+    if len(N_fluor2) > 0:
+        imsave(folder + os.sep + f"model_{fluor_names[1]}.tif", cumsum_fluor2 / sum(N_fluor2), plugin="tifffile")
+        color_img2 = np.zeros((30, 30, 3))
+        color_fluor2 = cellmodeler.assign_color(cumsum_fluor2 / sum(N_fluor2), color_img2, colormap)
+        imsave(folder + os.sep + f"color_{fluor_names[1]}_n{sum(N_fluor2)}.png", color_fluor2)
+
+    if len(N_fluor2_TM) > 0:
+        imsave(folder + os.sep + f"model_{fluor_names[1]}_TM_.tif", cumsum_fluor2_TM / sum(N_fluor2_TM), plugin="tifffile")
+        color_img2_TM = np.zeros((30, 30, 3))
+        color_fluor2_TM = cellmodeler.assign_color(cumsum_fluor2_TM / sum(N_fluor2_TM), color_img2_TM, colormap)
+        imsave(folder + os.sep + f"color_{fluor_names[1]}_TM_n{sum(N_fluor2_TM)}.png", color_fluor2_TM)
 
 
 if __name__ == '__main__':
 
-    for condition in os.listdir(r"C:\Users\António\Documents\30-11-2022_heatmaps"):
+    gui = MainGUI()
+    gui.mainloop()
 
-        path = os.path.join(r"C:\Users\António\Documents\30-11-2022_heatmaps", condition)
+    parameters = gui.pars
+
+    ehooke = parameters['ehookefolder']
+    rootfolder = parameters['rootfolder']
+    fluornames = [parameters['fluor1'], parameters['fluor2']]
+    basename = parameters['base']
+    basetype = parameters['basetype']
+
+    sys.path.append(ehooke)
+
+    from images import ImageManager
+    from parameters import MaskParameters, RegionParameters, CellParameters, ParametersManager
+    from segments import SegmentsManager
+    from cells import CellManager
+
+    for condition in os.listdir(rootfolder):
+
+        path = os.path.join(rootfolder, condition)
+
+        if not os.path.isdir(path):
+            continue
+
         print(condition)
-        run_batch_workflow(path)
-        join_replicates(path, "model_greater_1_TM_SPOTS")
-        join_replicates(path, "model_greater_1")
+        run_batch_workflow(path, fluornames, basename, basetype)
+        join_replicates(path, fluornames)
